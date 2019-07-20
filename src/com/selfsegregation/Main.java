e08d427d5c5425ee9daa125ea00d83497f9dff9d
Javad Khalili <gangester_bocho@yahoo.com>
2019-07-20 14:05:24 +0200

    Commit 3 : Agent and Square Lattice Construction Completed!

--- src/com/selfsegregation/Main.java
+++ src/com/selfsegregation/Main.java
@@ -1,7 +1,16 @@
 package com.selfsegregation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	
+	//=============================================================
+	public static double randomGenerator(int interval){
+		double random = (int)((Math.random() *   interval )) ; 
+		return random;
+	}
+	/*
 	public static double kronockerDelta(Node n1, Node n2){
 		double sum = 0;
 		for(int i = 0 ; i < n1.getF().size() ; i++){
@@ -11,10 +20,85 @@ public class Main {
 		}
 		return sum;
 	}
-	// at the end it should be divided by length of cultural traits
-	public static void main(String[] args) {
-		//int random = (int )(Math.random() * 50);
-		//System.out.println(random);
+	*/
+	//=============================================================
+	public static List<Node> generateAgents( int N ,int q ){
+		List<Node> agents = new ArrayList<>();
+		for(int n = 0 ; n < N ; n++){
+			agents.add( new Node(n , q ) );
+		}
+		System.out.println("Agents Generated.");
+		return agents;
 	}
+	//===============================================================
+	
+	public static Node[][] populateLattice(List<Node> agents , int L){
+		System.out.println("Constructing Lattice ...");
+		Node[][] square_lattice = new Node[L][L];
+		System.out.println("Populating Square_Lattice by Agents ... ");
+		System.out.println();
+		int row;
+		int column;
+		while(!(agents.isEmpty())){
+			row = (int)randomGenerator(L);
+			column = (int)randomGenerator(L);
+			if(square_lattice[row][column] == null){
+				square_lattice[row][column] = agents.get(0);
+				agents.remove(0);
+				}else continue;
+		}
+		System.out.println("The Lattice Has Been Populated by Agents.");
+		System.out.println();
+		return square_lattice;
+	}
+	//======================================================================
+	
+	public static void printLattic(Node[][] square_lattice , int L){
+		System.out.println("After Populating the Lattice , It's As Follow : ");
+		System.out.println();
+		for(int s = 0 ; s < square_lattice.length ; s++){
+			System.out.print("[ ");
+			for(int c = 0 ; c < square_lattice.length ; c++){
+				if(square_lattice[s][c] != null){
+				System.out.print(" "+square_lattice[s][c].getId()+" ");
+				}else{
+					System.out.print(" E ");
+				}
+			}
+			System.out.print(" ]");
+			System.out.println();
+		}
+	}
+	
+	//======================= Main Method ========================
+	public static void main(String[] args) {
+		
+		//============= Parameter Initialization ==================
+		int L = 5; 									// Lattice size
+		int q = 5 ;									// no. of cultures
+		double T; 									// Threshold
+		double h = 0.05; 							// Empty Sites Density in our Lattice
+		int N = (int)(( 1 - h ) * ( Math.pow(L,2)));// no. of agents
+		int F = 10;									// Cultural Feature of each Agent
+ 		//=========================================================
+				
+		List<Node> agents = generateAgents(N , q );
+		
+		//Prints Cultural Traits of Each Node
+		/*
+		for(int n = 0; n < agents.size(); n++){
+			System.out.print("Cultural Traits of Agent ( " + n + " ) are : "+" [ ");
+			for(int i = 0 ; i < agents.get(n).getF().size();i++){
+				System.out.print(" " + agents.get(n).getF().get(i)+ " ");
+			}
+			System.out.println(" ] ");
+			System.out.println();
+		}
+		*/
+		
+		Node[][] square_lattice = populateLattice( agents , L);
+		printLattic(square_lattice , L );
+		
+		}
 
 }
--- src/com/selfsegregation/Node.java
+++ src/com/selfsegregation/Node.java
@@ -1,5 +1,6 @@
 package com.selfsegregation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -9,7 +10,13 @@ public class Node implements Comparable<Node> {
 
 	private List<Integer> F;
 	
-	// no Constructor yet
+	public Node(int id , int q){
+		this.id = id;
+		this.F = new ArrayList<Integer>();
+		for(int i = 0 ; i < q ; i++){
+			F.add((int)(Math.random() * q) );
+		}
+	}
 	
 	public int getId() {
 		return id;
@@ -22,14 +29,23 @@ public class Node implements Comparable<Node> {
 	public List<Integer> getF() {
 		return F;
 	}
-
+/*
 	public void setF(List<Integer> cultural_trait) {
 		this.F = cultural_trait;
+	}
+*/	
+
+	
+
+	@Override
+	public String toString() {
+		return "Node [id=" + id + "]";
 	}
 
 	@Override
 	public int compareTo(Node node) {
 		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 }
